46b3b0c8270843723a71ae7ebb21c0c615c8257c
swapfile3 <swajeetg768@gmail.com>
2020-10-27 18:50:33 +0530

    Optimize for mobile

--- js/main.js
+++ js/main.js
@@ -13,35 +13,6 @@ window.onscroll = function () {
   }
 }
 
-/* tabs */
-function showTab(tabNumId) {
-  var tab = document.getElementById('window-overlay');
-  tab.className = "window tab-show";
-  /* TODO: populate with content */
-  var content = document.getElementById('content');
-  var title = document.getElementById('tab-title');
-  if (tabNumId === 0) {
-    title.innerHTML = "Web Development";
-    content.innerHTML = content.innerHTML + "<br>You clicked Web Development";
-  }
-  else if (tabNumId === 1) {
-    title.innerHTML = "Web Development";
-    content.innerHTML = content.innerHTML + "<br>You clicked Mobile Development";
-  }
-}
-
-function hideTab() {
-  var tab = document.getElementById('window-overlay');
-  tab.className = "window tab-hidden";
-  /* TODO: delete content */
-  var content = document.getElementById('content');
-  content.innerHTML = "Whatever the content we want";
-  var title = document.getElementById('tab-title');
-  title.innerHTML = "#TITLE";
-}
-
-
-
 
 $(document).ready(function () {
   $(window).scroll(function () {
